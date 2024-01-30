5047a17ff43f8bace13d6e56e8bc5e2fd4d24d36
bhargav232 <bhargavmanavadariya1998@gmail.com>
2024-01-30 15:35:49 -0500

    Adding assets and constant folder for image manupulation

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Navbar.scss"
+import {images} from "../../constants"
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div >
+      <img src = {images.logo} alt = "logo" />
     </div>
   )
 }
